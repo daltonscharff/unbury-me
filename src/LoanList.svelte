9500fa8390508917ff7f7ded46ba22c9b8967f1d
Dalton Scharff <dalton.scharff@gmail.com>
2021-06-01 20:51:40 -0500

    chanes loan list to accordian

--- src/LoanList.svelte
+++ src/LoanList.svelte
@@ -27,74 +27,104 @@
 	const deleteLoan = (i) => {
 		$loans = $loans.filter((_, index) => index != i);
 	};
+
+	const toggleVisibility = (e) => {
+		let target = e.target;
+		if (!target.classList.contains("title")) target = target.parentElement;
+		target.classList.toggle("active");
+		target.nextElementSibling.classList.toggle("active");
+	};
 </script>
 
-<div class="ui container">
-	<h1>Loans</h1>
-	<div class="ui cards">
-		{#each $loans as loan, i}
-		<div class="card">
-			<div class="content">
-				<span on:click={() => deleteLoan(i)} class="material-icons right floated icon">delete</span>
-				<div class="header">
-					{loan.name}
-				</div>
-				<div class="description">
-					<div>
-						<b>Principal: </b>
-						${loan.principal}
-					</div>
-					<div>
-						<b>Interest: </b>
-						{loan.interestPct}%
-					</div>
-					<div>
-						<b>Monthly Payment: </b>
-						${loan.minPayment}
-					</div>
-				</div>
+<div class="ui styled accordion">
+	{#each $loans as loan, i}
+		<div class="title" on:click={(event) => toggleVisibility(event)}>
+			<i class="dropdown icon" />
+			{loan.name}
+			<i class="icon trash alternate outline float-right" on:click={() => deleteLoan(i)} />
+		</div>
+		<div class="content">
+			<div>
+				<b>Principal: </b>
+				${loan.principal}
+			</div>
+			<div>
+				<b>Interest: </b>
+				{loan.interestPct}%
+			</div>
+			<div>
+				<b>Monthly Payment: </b>
+				${loan.minPayment}
 			</div>
 		</div>
-		{/each}
-		<div class="card">
-			<div class="content">
-				<form class="ui form">
-					<input type="text" placeholder="Loan Name" bind:value={newLoan.name} />
-					<input type="number" placeholder="Principal" bind:value={newLoan.principal} /> 
-					<input type="number" placeholder="Interest %" bind:value={newLoan.interestPct} /> 
-					<input type="number" placeholder="Minimum Payment" bind:value={newLoan.minPayment} />
-				</form>
-			</div>
-			<div class="ui bottom attracted button" on:click="{addNewLoan}">
+	{/each}
+	<div class="new-loan content active">
+		<form class="ui form" on:submit={(e) => e.preventDefault()}>
+			<input
+				type="text"
+				placeholder="Loan Name"
+				bind:value={newLoan.name}
+			/>
+			<input
+				type="number"
+				placeholder="Principal"
+				bind:value={newLoan.principal}
+			/>
+			<input
+				type="number"
+				placeholder="Interest %"
+				bind:value={newLoan.interestPct}
+			/>
+			<input
+				type="number"
+				placeholder="Minimum Payment"
+				bind:value={newLoan.minPayment}
+			/>
+			<button class="ui button" on:click={addNewLoan}>
 				Add Loan
-			</div>
-		</div>
+			</button>
+		</form>
 	</div>
 </div>
 
-<style type="text/scss">	
-.material-icons {
-	cursor: pointer;
-	font-size: 1.2em;
-	color: #333;
-}
+<style type="text/scss">
+	.accordion {
+		width: auto !important;
+	}
+	
+	.title {
+		color: black !important;
+	}
 
-.card {
-	flex-grow: 1;
-}
+	.float-right {
+		float: right;
+	}
 
-input {
-	border-top: 0 !important;
-	border-left: 0 !important;
-	border-right: 0 !important;
-	padding-left: .25em !important;
-}
+	.trash.icon {
+		padding: 0 15px 20px;
+	}
 
-input[type=number] {
-  -moz-appearance: textfield !important;
-}
+	.button {
+		margin: 2em auto auto;
+		width: 100%;
+	}
 
-::placeholder {
-	color: #333 !important;
-}
+	.new-loan:not(:first-child) {
+		border-top: 1px solid rgba(34,36,38,.15);
+	}
+
+	input {
+		border-top: 0 !important;
+		border-left: 0 !important;
+		border-right: 0 !important;
+		padding-left: 0.25em !important;
+	}
+
+	input[type="number"] {
+		-moz-appearance: textfield !important;
+	}
+
+	::placeholder {
+		color: black !important;
+	}
 </style>
