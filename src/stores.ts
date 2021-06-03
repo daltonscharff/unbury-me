f3c223fd150ae7ce790d86423a5d600df908711b
Dalton Scharff <dalton.scharff@gmail.com>
2021-06-02 21:52:49 -0500

    removes business logic from PaymentSchedule and moves it into a store

--- src/stores.ts
+++ src/stores.ts
@@ -1,5 +1,13 @@
 import { writable, Writable, derived, Readable } from 'svelte/store';
 
+type Month = Map<
+    string,
+    {
+        remainingPrincipal: number;
+        payment: number;
+    }
+>;
+
 export const loans: Writable<{ name: string, principal: number, interestPct: number, minPayment: number }[]> = writable([
     {
         name: "loan1",
@@ -18,5 +26,80 @@ export const monthlyPayment: Writable<number> = writable(0);
 
 export const paymentPlan: Writable<"avalanche" | "snowball"> = writable("avalanche");
 
-export const paymentSchedule: Writable<any[]> = writable([]);
+export const paymentSchedule = derived([loans, monthlyPayment, paymentPlan], ([$loans, $monthlyPayment, $paymentPlan]) => {
+    const sortedLoans = [...$loans].sort((a, b) => {
+        if ($paymentPlan === "avalanche") {
+            return a.interestPct < b.interestPct ? 1 : -1;
+        } else {
+            return a.principal > b.principal ? 1 : -1;
+        }
+    });
+
+    const createMonthZero = (loans) => {
+        let month: Month = new Map();
+        loans.forEach((loan) => {
+            month.set(loan.name, {
+                remainingPrincipal: loan.principal,
+                payment: 0,
+            });
+        });
+        return [month];
+    };
+
+    const calculatePayment = (loans, payment, months) => {
+        let month: Month = new Map();
+        let remainingPayment = payment;
+
+        for (let loan of loans) {
+            let principalLastMonth = months[months.length - 1].get(loan.name).remainingPrincipal;
+            let interest = loan.interestPct * 0.01 / 12 * principalLastMonth;
+            principalLastMonth += interest;
+            let record = {
+                remainingPrincipal: principalLastMonth - loan.minPayment,
+                payment: principalLastMonth < loan.minPayment ? principalLastMonth : loan.minPayment,
+            };
+            if (record.remainingPrincipal < 0) {
+                payment += record.remainingPrincipal;
+                record.remainingPrincipal = 0;
+            }
+            month.set(loan.name, record);
+            remainingPayment -= record.payment;
+        }
+
+        for (let loan of loans) {
+            let record = month.get(loan.name);
+            record.remainingPrincipal -= remainingPayment;
+            record.payment += remainingPayment;
+            remainingPayment = 0;
+            if (record.remainingPrincipal <= 0) {
+                record.payment += record.remainingPrincipal;
+                remainingPayment -= record.remainingPrincipal;
+                record.remainingPrincipal = 0;
+            }
+            month.set(loan.name, record);
+        }
+
+        months = [...months, month];
+        return months;
+    };
+
+    const generatePaymentSchedule = (loans, payment) => {
+        let months = createMonthZero(loans);
+
+        for (let i = 0; i < 100; i++) {
+            months = calculatePayment(loans, payment, months);
+            let currentMonth = months[months.length - 1];
+            let remaining = 0;
+            for (let loanName of currentMonth.keys()) {
+                remaining += currentMonth.get(loanName).remainingPrincipal;
+            }
+
+            if (remaining === 0) break;
+        }
+        return months;
+    };
+
+    return generatePaymentSchedule(sortedLoans, $monthlyPayment);
+});
+
 
