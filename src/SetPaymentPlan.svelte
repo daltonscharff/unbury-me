a803f06c2054b706ed0351e3c15b1f45b4c0d51f
Dalton Scharff <dalton.scharff@gmail.com>
2021-06-05 12:00:28 -0500

    adds spacing to set payment plan container

--- src/SetPaymentPlan.svelte
+++ src/SetPaymentPlan.svelte
@@ -12,44 +12,54 @@
     $: $monthlyPayment = minimumMonthlyPayment;
 </script>
 
-<div class="ui form">
-    <div class="grouped fields">
-        <div class="field">
-            <div class="ui radio">
-                <label>
-                    <input
-                        type="radio"
-                        bind:group={$paymentPlan}
-                        value="avalanche"
-                    />
-                    Highest Interest Rate (Avalanche)
-                </label>
+<div class="container">
+    <div class="ui form">
+        <div class="grouped fields">
+            <div class="field">
+                <div class="ui radio">
+                    <label>
+                        <input
+                            type="radio"
+                            bind:group={$paymentPlan}
+                            value="avalanche"
+                        />
+                        Highest Interest Rate (Avalanche)
+                    </label>
+                </div>
             </div>
-        </div>
-        <div class="field">
-            <div class="ui radio">
-                <label>
-                    <input
-                        type="radio"
-                        bind:group={$paymentPlan}
-                        value="snowball"
-                    />
-                    Lowest Principal (Snowball)
-                </label>
+            <div class="field">
+                <div class="ui radio">
+                    <label>
+                        <input
+                            type="radio"
+                            bind:group={$paymentPlan}
+                            value="snowball"
+                        />
+                        Lowest Principal (Snowball)
+                    </label>
+                </div>
             </div>
         </div>
     </div>
+    <p>Monthly Payment: ${$monthlyPayment}</p>
+    <input
+        type="range"
+        bind:value={$monthlyPayment}
+        min={minimumMonthlyPayment}
+        max={maximumMonthlyPayment}
+    />
 </div>
-<p>monthly payment: {$monthlyPayment}</p>
-<input
-    type="range"
-    bind:value={$monthlyPayment}
-    min={minimumMonthlyPayment}
-    max={maximumMonthlyPayment}
-/>
 
 <style>
 input[type=radio] {
     margin-right: .5em;
+}
+
+input[type=range] {
+    width: 100%;
+}
+
+.container {
+    margin-bottom: 1em;
 }
 </style>
